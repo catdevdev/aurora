a4df86806a714c8c1613727c668e652b3dcffdd5
Vladislav Oliinyk <catdev.dev@gmail.com>
2022-07-20 03:27:55 +0300

    added

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '123 123 Hello World! 123 123';
+    return 'Hello World!';
   }
 }
